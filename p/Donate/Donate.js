120e25a080791939e57b12d77d3472e0011bd952
khalid Elabd <113540126+khalidKE@users.noreply.github.com>
2024-05-15 08:24:48 +0200

    Add files via upload

---
+++ p/Donate/Donate.js
@@ -0,0 +1,18 @@
+function showPaymentDetails(paymentMethod) {
+    var visaDetails = document.getElementById("visa-details");
+    var vodafoneCashDetails = document.getElementById("vodafone-cash-details");
+    
+    if (paymentMethod === 'visa') {
+      visaDetails.style.display = "block";
+      vodafoneCashDetails.style.display = "none";
+    } else if (paymentMethod === 'vodafone-cash') {
+      visaDetails.style.display = "none";
+      vodafoneCashDetails.style.display = "block";
+    } else {
+      visaDetails.style.display = "none";
+      vodafoneCashDetails.style.display = "none";
+    }
+}
+
+
+    alert('Great job, you will save the lives of innocent people');
